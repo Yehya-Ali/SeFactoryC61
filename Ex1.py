7cf1ad602d8b60e5af5a387b3206dd7e809e403a
Yehya-Ali <yehya.z.ali@gmail.com>
2025-02-13 04:09:01 +0200

    This is Excersice 1

--- Ex1.py
+++ Ex1.py
@@ -1,8 +1,9 @@
 age = int(input("Enter your age: "))
 
-if age <= 8:
-    print("The ticket price is $5")
-elif age > 8 and age <= 15:
-    print("The ticket price is $10")
-else:
-    print("The ticket price is $15")
+while True:
+    if age <= 8:
+        print("The ticket price is $5")
+    elif age > 8 and age <= 15:
+        print("The ticket price is $10")
+    else:
+        print("The ticket price is $15")
